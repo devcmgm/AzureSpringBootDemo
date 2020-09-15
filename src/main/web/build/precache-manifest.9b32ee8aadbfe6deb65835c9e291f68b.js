2c9b6d588e83a7a7a973df0f6ceff53d6ced601b
z <zlewis.guest@kr.af.mil>
2020-09-15 12:15:04 -0500

    add webcode

---
+++ src/main/web/build/precache-manifest.9b32ee8aadbfe6deb65835c9e291f68b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "993bbb5b7a136f219b1d056899164673",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22094703377b034023b7",
+    "url": "/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "ffda34061473d67c94e6",
+    "url": "/static/js/2.c656d006.chunk.js"
+  },
+  {
+    "revision": "436406a2a77e5a062880b8a01eb69ba0",
+    "url": "/static/js/2.c656d006.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22094703377b034023b7",
+    "url": "/static/js/main.b23d2ecf.chunk.js"
+  },
+  {
+    "revision": "9044ab9180ae661a8c66",
+    "url": "/static/js/runtime-main.545fa6c6.js"
+  },
+  {
+    "revision": "357727842afd08b35401bb484f49ac63",
+    "url": "/static/media/birds.35772784.jpg"
+  },
+  {
+    "revision": "357727842afd08b35401bb484f49ac63",
+    "url": "/static/media/books.35772784.jpg"
+  },
+  {
+    "revision": "357727842afd08b35401bb484f49ac63",
+    "url": "/static/media/creative.35772784.jpg"
+  },
+  {
+    "revision": "357727842afd08b35401bb484f49ac63",
+    "url": "/static/media/mainsmile.35772784.jpg"
+  }
+]);
